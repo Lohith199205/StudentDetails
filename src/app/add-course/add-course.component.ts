1e6959cade0ddae2fc949d0942c3653f423e5cd2
lohith199205 <lali199205@gmail.com>
2019-07-03 19:25:23 +0530

    done

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -9,27 +9,31 @@ import { AdminDashboardService } from '../admin-dashboard.service';
 })
 export class AddCourseComponent implements OnInit {
 
-  
+  courseData: FormGroup;
+  diaplayVariable : "none";
+  submitted:boolean = false;
   constructor(private fb : FormBuilder,private service:AdminDashboardService) { }
 
-  courseData = this.fb.group({
-    courseName:['',[Validators.required, Validators.minLength(5)] ],
-    courseTitle:['',[Validators.required, Validators.minLength(5)]],
-    courseMetaTag:['',[Validators.required, Validators.minLength(5)]],
-    courseMetaDescription:['',[Validators.required, Validators.minLength(5)]],
-    courseOverview:['',[Validators.required, Validators.minLength(5)]]
-  });
-
   ngOnInit() {
+    this.courseData = this.fb.group({
+      courseName:['',[Validators.required, Validators.minLength(5)] ],
+      courseTitle:['',[Validators.required, Validators.minLength(5)]],
+      courseMetaTag:['',[Validators.required, Validators.minLength(5)]],
+      courseMetaDescription:['',[Validators.required, Validators.minLength(5)]],
+      courseOverview:['',[Validators.required, Validators.minLength(5)]]
+    });
   }
+  get f() { return this.courseData.controls; }
 
   storeData(){
+   this.submitted=true;
+
     if(this.courseData.valid)
     {
       console.log(this.courseData.value);
      // this.service.postData(this.courseData.value);
     }else{
-      console.log("Please enter valid form data");
+      console.log("Please enter valid form data"+this.f.courseName.invalid+"  "+this.submitted+"   "+this.f.courseName.errors.required);
     }
     
   }
